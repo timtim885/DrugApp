8cfddd30fd583efd6439e1c8b3ff77ec3cf74029
phsit <phsit@dhcp-10-10-71-64.phs.pacific.edu>
2014-05-15 16:54:51 -0700

    Autolayout bs

---
+++ Pharm/DrugSearchRootViewController.h
@@ -0,0 +1,14 @@
+//
+//  DrugSearchRootViewController.h
+//  Pharm
+//
+//  Created by phsit on 5/15/14.
+//  Copyright (c) 2014 UOP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrugSearchRootViewController : UIViewController <UITabBarDelegate>
+@property (weak, nonatomic) IBOutlet UITabBar *tabBar;
+
+@end
---
+++ Pharm/DrugSearchRootViewController.m
@@ -0,0 +1,55 @@
+//
+//  DrugSearchRootViewController.m
+//  Pharm
+//
+//  Created by phsit on 5/15/14.
+//  Copyright (c) 2014 UOP. All rights reserved.
+//
+
+#import "DrugSearchRootViewController.h"
+
+@interface DrugSearchRootViewController ()
+
+@end
+
+@implementation DrugSearchRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item{
+    
+    NSLog(@"selected %@", item);
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
